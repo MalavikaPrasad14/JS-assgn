b0452db05a4f57ebbc38d655c52c9bfd8376e1be
MalavikaPrasad14 <malavika0987offc@gmail.com>
2024-09-02 00:08:21 +0530

    second commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // QUES 1
 
-console.log("Write a JavaScript function to get an array and to read the first element of an array. Check whether it is prime or not.");
+console.log("1) Write a JavaScript function to get an array and to read the first element of an array. Check whether it is prime or not.");
 
 let A = [10, 13, 2, 25, 4, 7];
 let num = A[0];
@@ -14,18 +14,18 @@ function isPrime(num) {
         console.log(n + " is a prime number");
     }
     if (num % 2 === 0) {
-        console.log(n + " is not a prime number");
+        console.log(num + " is not a prime number");
     }
 
     for (let i = 3; i <= Math.sqrt(num); i += 2) {
         if (num % i === 0) return false; // If divisible by any number other than 1 and itself, not prime
     }
 
-    return true; // If no divisors found, it's prime
+    // return true; // If no divisors found, it's prime
 }
 
 // QUES 2
-console.log(" Write a JavaScript program to find the most frequent item of an array");
+console.log("2) Write a JavaScript program to find the most frequent item of an array");
 
 function mostFrequentItem(A) {
     if (A.length === 0) return null;
@@ -54,7 +54,7 @@ console.log("The most frequent item is: " + ans);
 //QUES 3
 
 
-console.log("Write a JavaScript for loop that will iterate from 0 to 15. For each iteration, check if the current number is odd or even, and display the message to the screen as odd or even.");
+console.log("3) Write a JavaScript for loop that will iterate from 0 to 15. For each iteration, check if the current number is odd or even, and display the message to the screen as odd or even.");
 for (let i = 0; i <= 15; i++) {
     if (i % 2 === 0) {
         console.log(i + "is Even number");
@@ -66,7 +66,7 @@ for (let i = 0; i <= 15; i++) {
 
 
 // QUES 4
-console.log("Write a JavaScript program to find the sum of squares of the elements of an array.");
+console.log("4) Write a JavaScript program to find the sum of squares of the elements of an array.");
 
 function sumOfSqr(array) {
     let sum = 0;
